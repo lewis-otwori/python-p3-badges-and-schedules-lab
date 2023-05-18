4475c1e1004ab0dfffc29e0a3438795ea0f88490
lewis-otwori <lewisotwori@gmail.com>
2023-05-18 11:08:03 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return  f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return  [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {i}!" for i, name in enumerate(names, start=1)]
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print (badge)
+
+    for room in rooms:
+        print (room)
+    # return (badges, rooms)
+print(printer(["Arel", "Carol"]))
